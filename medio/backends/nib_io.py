c1cfff61718074231e5a67dae16e2ae3eeb0c449
Jonathan Daniel <jonathan@rsipvision.com>
2020-05-30 23:20:32 +0300

    Added channels support in nib_io (RGB[A])
    With channels_axis keyword argument (reading and saving)

--- medio/backends/nib_io.py
+++ medio/backends/nib_io.py
@@ -9,14 +9,21 @@ from medio.metadata.metadata import MetaData
 
 class NibIO:
     coord_sys = 'nib'
+    RGB_DTYPE = np.dtype([('R', np.uint8),
+                          ('G', np.uint8),
+                          ('B', np.uint8)])
+    RGBA_DTYPE = np.dtype([('R', np.uint8),
+                           ('G', np.uint8),
+                           ('B', np.uint8),
+                           ('A', np.uint8)])
 
     @staticmethod
-    def read_img(input_path, desired_axcodes=None, unravel=False, header=False):
+    def read_img(input_path, desired_axcodes=None, channels_axis=None, header=False):
         """
         Reads a NIFTI file and returns the image array and metadata
         :param input_path: path-like (str or pathlib.Path) of the image file to read
         :param desired_axcodes: str, tuple of str or None - the desired orientation of the image to be returned
-        :param unravel: if True and the array dtype is structured, stacks the channels along the last axis
+        :param channels_axis: if not None and the array dtype is structured, stacks the channels along the components_axis
         :param header: whether to include a header attribute with additional metadata in the returned metadata
         :return: image array and corresponding metadata
         """
@@ -25,8 +32,8 @@ class NibIO:
         if desired_axcodes is not None:
             img_struct = NibIO.reorient(img_struct, desired_axcodes)
         img = np.asanyarray(img_struct.dataobj)
-        if unravel:
-            img = NibIO.unravel_array(img)
+        if channels_axis is not None:
+            img = NibIO.unravel_array(img, channels_axis)
         affine = Affine(img_struct.affine)
         metadata = MetaData(affine=affine, orig_ornt=orig_ornt_str, coord_sys=NibIO.coord_sys)
         if header:
@@ -34,14 +41,17 @@ class NibIO:
         return img, metadata
 
     @staticmethod
-    def save_img(filename, img, metadata, use_original_ornt=True):
+    def save_img(filename, img, metadata, use_original_ornt=True, channels_axis=None):
         """
         Saves the given image as a NIFTI file.
         :param filename: path-like output filename, including a '.nii.gz' or '.nii' suffix
         :param img: image data array
         :param metadata: the matching metadata
         :param use_original_ornt: whether to use the original orientation of the image of not
+        :param channels_axis: if not None gives the channels axis of img (for channeled images RGB/RGBA)
         """
+        if channels_axis is not None:
+            img = NibIO.pack_channeled_img(img, channels_axis)
         orig_coord_sys = metadata.coord_sys
         metadata.convert(NibIO.coord_sys)
         img_struct = nib.Nifti1Image(img, metadata.affine)
@@ -64,11 +74,25 @@ class NibIO:
         return img_struct
 
     @staticmethod
-    def unravel_array(array):
+    def unravel_array(array, channels_axis=-1):
         """Simplify array dtype if it is a structured data type. For example, if the array if of RGB dtype:
         np.dtype([('R', 'uint8'), ('G', 'uint8'), ('B', 'uint8')])
         Convert it into an array with dtype 'uint8' and 3 channels for RGB in an additional last dimension"""
         dtype = array.dtype
         if not (hasattr(dtype, '__len__') and len(dtype) > 1):
             return array
-        return np.stack([array[field] for field in dtype.fields], axis=-1)
+        return np.stack([array[field] for field in dtype.fields], axis=channels_axis)
+
+    @staticmethod
+    def pack_channeled_img(img, channels_axis):
+        dtype = img.dtype
+        if not np.issubdtype(dtype, np.uint8):
+            raise ValueError(f'RGB or RGBA images must have dtype "np.uint8", got: "{dtype}"')
+        n_channels = img.shape[channels_axis]
+        img = np.moveaxis(img, channels_axis, -1)
+        if n_channels == 3:
+            return img.view(NibIO.RGB_DTYPE)
+        elif n_channels == 4:
+            return img.view(NibIO.RGBA_DTYPE)
+        else:
+            raise ValueError(f'Invalid number of channels: {n_channels}, should be 3 (RGB) or 4 (RGBA)')
