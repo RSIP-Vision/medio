5f8976c93a723690cd6334b9413aa0ba317f1a7f
Jonathan Daniel <jonathan@rsipvision.com>
2020-05-30 14:03:58 +0300

    Removed py3.9 from classifiers (not released yet)

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(name='medio',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
 
             'Operating System :: OS Independent'
       ],
