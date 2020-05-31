1a7d62f3786458834d06d0ebdef6294eca19cd62
Jonathan Daniel <jonathan@rsipvision.com>
2020-05-31 09:20:11 +0300

    Added channels_axis and header to read_save funcs.

--- medio/read_save.py
+++ medio/read_save.py
@@ -7,7 +7,7 @@ from medio.metadata.convert_nib_itk import inv_axcodes
 from medio.utils.files import is_nifti
 
 
-def read_img(input_path, desired_ornt=None, backend=None, dtype=None, header=False, **kwargs):
+def read_img(input_path, desired_ornt=None, backend=None, dtype=None, header=False, channels_axis=-1, **kwargs):
     """
     Read medical image with nibabel or itk
     :param input_path: str or os.PathLike, the input path of image file or a directory containing dicom series
@@ -17,6 +17,8 @@ def read_img(input_path, desired_ornt=None, backend=None, dtype=None, header=Fal
     :param dtype: equivalent to np_image.astype(dtype) if dtype is not None
     :param header: if True, the returned metadata will include a header attribute with additional metadata dictionary as
     read by the backend. Note: currently, this is supported for files only
+    :param channels_axis: if not None and the image is channeled (e.g. RGB) move the channels to channels_axis in the
+    returned image array
     :return: numpy image and metadata object
     """
     nib_reader = NibIO.read_img
@@ -44,17 +46,17 @@ def read_img(input_path, desired_ornt=None, backend=None, dtype=None, header=Fal
         desired_ornt = inv_axcodes(desired_ornt)
 
     if reader is pdcm_reader:
-        np_image, metadata = reader(input_path, header=header, **kwargs)
+        np_image, metadata = reader(input_path, header, channels_axis, **kwargs)
     else:
-        np_image, metadata = reader(input_path, desired_axcodes=desired_ornt, header=header, **kwargs)
+        np_image, metadata = reader(input_path, desired_ornt, header, channels_axis, **kwargs)
 
     if dtype is not None:
         np_image = np_image.astype(dtype)
     return np_image, metadata
 
 
-def save_img(filename, np_image, metadata, use_original_ornt=True, backend=None, dtype=None, mkdir=False, parents=False,
-             **kwargs):
+def save_img(filename, np_image, metadata, use_original_ornt=True, backend=None, dtype=None, channels_axis=None,
+             mkdir=False, parents=False, **kwargs):
     """
     Save numpy image with corresponding metadata to file
     :param filename: str or os.PathLike, the output filename
@@ -63,6 +65,7 @@ def save_img(filename, np_image, metadata, use_original_ornt=True, backend=None,
     :param use_original_ornt: whether to save in the original orientation stored in metadata.orig_ornt or not
     :param backend: optional - 'itk', 'nib' or None
     :param dtype: equivalent to np_image.astype(dtype) if dtype is not None
+    :param channels_axis: if not None - the image is channeled (e.g. RGB) and the channels are in channels_axis
     :param mkdir: if True, creates the directory of `filename`
     :param parents: to be used with `mkdir=True`. If True, creates also the parent directories
     """
@@ -84,13 +87,14 @@ def save_img(filename, np_image, metadata, use_original_ornt=True, backend=None,
         Path(filename).parent.mkdir(parents=parents, exist_ok=True)
     if dtype is not None:
         np_image = np_image.astype(dtype)
-    writer(filename, np_image, metadata, use_original_ornt, **kwargs)
+    writer(filename, np_image, metadata, use_original_ornt, channels_axis, **kwargs)
 
 
-def save_dir(dirname, np_image, metadata, use_original_ornt=True, dtype=None, is_vector=False, parents=False,
+def save_dir(dirname, np_image, metadata, use_original_ornt=True, dtype=None, channels_axis=None, parents=False,
              allow_dcm_reorient=False, **kwargs):
     """Save image as a dicom directory. See medio.backends.itk_io.ItkIO.save_dcm_dir documentation.
     dtype is equivalent to passing image_np.astype(dtype) if dtype is not None"""
     if dtype is not None:
         np_image = np_image.astype(dtype)
-    ItkIO.save_dcm_dir(dirname, np_image, metadata, use_original_ornt, is_vector, parents, allow_dcm_reorient, **kwargs)
+    ItkIO.save_dcm_dir(dirname, np_image, metadata, use_original_ornt, channels_axis, parents, allow_dcm_reorient,
+                       **kwargs)
