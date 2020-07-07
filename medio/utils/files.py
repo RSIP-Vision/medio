4503103bf819adec8d8df3e0bef9230f46f83817
Jonathan Daniel <jonathan@rsipvision.com>
2020-07-07 21:26:54 +0300

    Use tuple of suffixes in endswith (is_file_suffix)

--- medio/utils/files.py
+++ medio/utils/files.py
@@ -2,20 +2,24 @@ from pathlib import Path
 
 
 def is_file_suffix(filename, suffixes, check_exist=True):
+    """
+    is_file + check for suffix
+    :param filename: pathlike object
+    :param suffixes: tuple of possible suffixes
+    :param check_exist: whether to check the file's existence
+    :return: bool
+    """
     if check_exist and not Path(filename).is_file():
         return False
-    for suf in suffixes:
-        if str(filename).endswith(suf):
-            return True
-    return False
+    return str(filename).endswith(suffixes)
 
 
 def is_nifti(filename, check_exist=True):
-    return is_file_suffix(filename, ['.nii.gz', '.nii', '.img.gz', '.img', '.hdr'], check_exist=check_exist)
+    return is_file_suffix(filename, ('.nii.gz', '.nii', '.img.gz', '.img', '.hdr'), check_exist=check_exist)
 
 
 def is_dicom(filename, check_exist=True):
-    return is_file_suffix(filename, ['.dcm', '.dicom', '.DCM', '.DICOM'], check_exist=check_exist)
+    return is_file_suffix(filename, ('.dcm', '.dicom', '.DCM', '.DICOM'), check_exist=check_exist)
 
 
 def make_empty_dir(dir_path, parents=False):
