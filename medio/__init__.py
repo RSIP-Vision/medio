c90097783e78c81a2e92a2d41802f178c71cb929
Jonathan Daniel <jonathan@rsipvision.com>
2020-07-24 17:45:22 +0300

    Version 0.3.0
    First fully open source version

--- medio/__init__.py
+++ medio/__init__.py
@@ -3,6 +3,6 @@ from medio.metadata.metadata import MetaData
 from medio.read_save import read_img, save_img, save_dir
 from medio import backends, metadata, medimg, utils
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 __all__ = ['read_img', 'save_img', 'save_dir', 'MetaData', 'Affine', '__version__']
