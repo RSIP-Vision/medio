a8035b5885d91585216f00a1bac148d2824fce66
Jonathan Daniel <jonathan@rsipvision.com>
2021-02-18 18:16:52 +0200

    Updated the dependencies for ITK 5.2
    ITK 5.2rc2 is only available via pip currently.
    Update to itk >= 5.2 when it is officially released.
    Installing itk-io and itk-filtering independently is no longer
    supported (?), check later. The install is also much faster, so maybe
    there is no need to be extra specific with itk's sub-packages.
    Added Python 3.9 to the classifiers (all dependencies agree on the
    python versions).

--- setup.py
+++ setup.py
@@ -17,8 +17,7 @@ setup(name='medio',
       keywords=['medical-images', 'IO', 'itk', 'nibabel', 'pydicom', 'python'],
       packages=find_packages(exclude=['*.tests']),
       install_requires=[
-            'itk-io == 5.0.1',
-            'itk-filtering == 5.0.1',
+            'itk >= 5.2rc2',
             'nibabel >= 3.0.2',
             'pydicom >= 2.0.0',
             'dicom-numpy >= 0.4.0',
@@ -40,6 +39,7 @@ setup(name='medio',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
 
             'Operating System :: OS Independent'
       ],
