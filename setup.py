e7c4814144bf1b76faeed3a3245c994ccc89d5e5
Jonathan Daniel <jonathand655@gmail.com>
2021-06-20 09:36:13 +0300

    Modernize the __version__ search in setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
-import codecs
-import os.path
-
 from setuptools import setup, find_packages
 
 
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-
-
 def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
+    with open(rel_path, 'r') as fp:
+        for line in fp:
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+        else:
+            raise RuntimeError("Unable to find version string.")
 
 
 with open('README.md') as f:
